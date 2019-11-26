cb31fd2dd165e989c151bf2f6cee6c9c93d31613
Niels van Dijk <info@vandijk-vormgeving.nl>
2019-11-26 16:12:35 +0100

    Added show and grab_coffee

--- app/controllers/api/coffees_controller.rb
+++ app/controllers/api/coffees_controller.rb
@@ -2,4 +2,14 @@ class Api::CoffeesController < ApplicationController
   def index
     render json: Coffee.all
   end
+
+  def show
+    @current_coffee = Coffee.find(params[:id])
+    render json: @current_coffee
+  end
+
+  def grab_coffee
+    @current_coffee = Coffee.find(params[:coffee_id]).id
+    Coffee.find(@current_coffee).decrement!(:amount, 1)
+  end
 end
