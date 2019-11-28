1574a9f4567c78970052c39314e94d6d3958cd37
Niels van Dijk <info@vandijk-vormgeving.nl>
2019-11-28 08:33:22 +0100

    Remove the @ in current_coffee

--- app/controllers/api/coffees_controller.rb
+++ app/controllers/api/coffees_controller.rb
@@ -4,12 +4,12 @@ class Api::CoffeesController < ApplicationController
   end
 
   def show
-    @current_coffee = Coffee.find(params[:id])
-    render json: @current_coffee
+    current_coffee = Coffee.find(params[:id])
+    render json: current_coffee
   end
 
   def grab_coffee
-    @current_coffee = Coffee.find(params[:coffee_id]).id
-    Coffee.find(@current_coffee).decrement!(:amount, 1)
+    current_coffee = Coffee.find(params[:coffee_id]).id
+    Coffee.find(current_coffee).decrement!(:amount, 1)
   end
 end
