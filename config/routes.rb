fa3af571d8fab8956f8def045246c5c2d4f826bc
Niels van Dijk <info@vandijk-vormgeving.nl>
2019-11-28 08:30:07 +0100

    Change first route to coffee index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api do
+  root to: 'api/coffees#index'
+
+  namespace :api, defaults: { format: :json } do
     resources :coffees, only:[:index, :create, :show] do
       get :grab_coffee
     end
